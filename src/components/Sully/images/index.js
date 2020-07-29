bc2e5a2db8d2546b7fb7c4d0db5003f029f38fa2
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2020-07-29 11:23:38 +0100

    cra rewrite

---
+++ src/components/Sully/images/index.js
@@ -0,0 +1,69 @@
+import BandanaSm from './Bandana-320.jpeg'
+import BandanaLg from './Bandana-800.jpeg'
+import BeachSm from './Beach-320.jpeg'
+import BeachLg from './Beach-800.jpeg'
+import Catdog2Sm from './Catdog-2-320.jpeg'
+import Catdog2Lg from './Catdog-2-800.jpeg'
+import CatdogSm from './Catdog-320.jpeg'
+import CatdogLg from './Catdog-800.jpeg'
+import ChristmasSm from './Christmas-320.jpeg'
+import ChristmasLg from './Christmas-800.jpeg'
+import CuddleSm from './Cuddle-320.jpeg'
+import CuddleLg from './Cuddle-800.jpeg'
+import DogsSm from './Dogs-320.jpeg'
+import DogsLg from './Dogs-800.jpeg'
+import FudSm from './Fud-320.jpeg'
+import FudLg from './Fud-800.jpeg'
+import HillsSm from './Hills-320.jpeg'
+import HillsLg from './Hills-800.jpeg'
+import HoseSm from './hose-320.jpeg'
+import HoseLg from './hose-800.jpeg'
+import LushSm from './Lush-320.jpeg'
+import LushLg from './Lush-800.jpeg'
+import MonkSm from './Monk-320.png'
+import MonkLg from './Monk-800.png'
+import PorkbuttsSm from './Porkbutts-320.jpeg'
+import PorkbuttsLg from './Porkbutts-800.jpeg'
+import RollSm from './Roll-320.jpeg'
+import RollLg from './Roll-800.jpeg'
+import Sleep2Sm from './Sleep-2-320.jpeg'
+import Sleep2Lg from './Sleep-2-800.jpeg'
+import SleepSm from './Sleep-320.jpeg'
+import SleepLg from './Sleep-800.jpeg'
+import SnowSm from './Snow-320.jpeg'
+import SnowLg from './Snow-800.jpeg'
+import SteakSm from './Steak-320.jpeg'
+import SteakLg from './Steak-800.jpeg'
+import SunsetSm from './Sunset-320.jpeg'
+import SunsetLg from './Sunset-800.jpeg'
+import SunsetXL from './Sunset-1600.jpeg'
+import ToysSm from './Toys-320.jpeg'
+import ToysLg from './Toys-800.jpg'
+import TvSm from './tv-320.png'
+import TvLg from './tv-800.png'
+
+export default [
+  { name: 'bandana', small: BandanaSm, large: BandanaLg, alt: 'Sully in a bandana', text: 'He always liked dressing up' },
+  { name: 'beach', small: BeachSm, large: BeachLg, alt: 'Sully at the beach', text: 'His favourite place in the world was the beach' },
+  { name: 'catdog2', small: Catdog2Sm, large: Catdog2Lg, alt: 'Sully and Roxy', text: 'His best friend was Roxy Cat' },
+  { name: 'catdog', small: CatdogSm, large: CatdogLg, alt: 'Sully and Roxy', text: 'Best pals forever' },
+  { name: 'christmas', small: ChristmasSm, large: ChristmasLg, alt: 'Sully and Santa', text: 'One time he almost saw Santa' },
+  { name: 'cuddle', small: CuddleSm, large: CuddleLg, alt: 'Having a cuddle', text: 'He loved cuddles' },
+  { name: 'dogs', small: DogsSm, large: DogsLg, alt: 'Dogs', text: 'He was always playful' },
+  { name: 'fud', small: FudSm, large: FudLg, alt: 'Food', text: 'He loved his food' },
+  { name: 'hills', small: HillsSm, large: HillsLg, alt: 'hills', text: 'He was always exploring' },
+  { name: 'hose', small: HoseSm, large: HoseLg, alt: 'hose', text: 'The hose was his arch-nemesis' },
+  { name: 'lush', small: LushSm, large: LushLg, alt: 'lush', text: 'He was devilishly handsome' },
+  { name: 'monk', small: MonkSm, large: MonkLg, alt: 'monk', text: 'He had the odd dodgy haircut' },
+  { name: 'porkbutts', small: PorkbuttsSm, large: PorkbuttsLg, alt: 'porkbutts', text: 'He loved chewing on Dr Porkbutts' },
+  { name: 'roll', small: RollSm, large: RollLg, alt: 'Sully chewing on a loo roll', text: 'He was always chewing cardboard' },
+  { name: 'sleep2', small: Sleep2Sm, large: Sleep2Lg, alt: 'Sully sleeping on the settee', text: 'At the end of the day, he loved to sleep'},
+  { name: 'sleep', small: SleepSm, large: SleepLg, alt: 'Sully sleeping on the bed', text: 'Sleep tight buddy' },
+  { name: 'snow', small: SnowSm, large: SnowLg, alt: 'Sully in the snow', text: 'He had to wait a few years to see it, but he loved the snow' },
+  { name: 'steak', small: SteakSm, large: SteakLg, alt: 'Sully eating a steak', text: 'Steak was his favourite' },
+  { name: 'toys', small: ToysSm, large: ToysLg, alt: 'Sully with a toy', text: 'He loved his toys until the end' },
+  { name: 'tv', small: TvSm, large: TvLg, alt: 'Sully with the zappa', text: 'He was always in charge of what we watched' }
+  // SunsetSm,
+  // SunsetLg,
+  // SunsetXL
+]
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import withReduxFeatures from './withReduxFeatures'
+import App from './components/App'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+/** Wrap App component with store providers */
+const WrappedApp = withReduxFeatures(App)
+
+ReactDOM.render(<WrappedApp />, document.getElementById('root'))
+
+/**
+ * If you want your app to work offline and load faster,
+ * you can change unregister() to register() below.
+ * Note this comes with some pitfalls.
+ * @see https://bit.ly/CRA-PWA
+ */
+serviceWorker.unregister()
