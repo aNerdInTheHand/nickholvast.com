065ebe7f0283317afd0bca9858fdd44898f4193a
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2022-06-14 16:07:49 +0100

    function tidy up

--- scripts/rumours.js
+++ scripts/rumours.js
@@ -1,13 +1,8 @@
 function getRumour () {
-  const options = {
-    headers: new Headers({ 'x-api-key': 'uw5ICBVlhV3Omu1b3NkQg1tw47WXDaK85bEfme7q' })
-  }
   fetch('https://00xzeci8z8.execute-api.eu-west-1.amazonaws.com')
     .then(response => {
-      console.log(response)
       response.json()
         .then(rumour => {
-          console.log(rumour)
           document.getElementById('rumour').innerHTML = rumour.replace(/"/g, "'")
         })
     })
