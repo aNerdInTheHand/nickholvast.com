25c88544595237382c1e293f42fcf36772a15202
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2022-06-14 15:56:15 +0100

    remove options

--- scripts/rumours.js
+++ scripts/rumours.js
@@ -2,7 +2,7 @@ function getRumour () {
   const options = {
     headers: new Headers({ 'x-api-key': 'uw5ICBVlhV3Omu1b3NkQg1tw47WXDaK85bEfme7q' })
   }
-  fetch('https://00xzeci8z8.execute-api.eu-west-1.amazonaws.com', options)
+  fetch('https://00xzeci8z8.execute-api.eu-west-1.amazonaws.com')
     .then(response => {
       console.log(response)
       response.json()
