2254886c102cda5db9d0422cbfa663234d08fba5
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2022-06-14 14:51:30 +0100

    update rumour script

--- scripts/rumours.js
+++ scripts/rumours.js
@@ -1,8 +1,7 @@
 function getRumour () {
   fetch('https://00xzeci8z8.execute-api.eu-west-1.amazonaws.com')
     .then(response => {
-      response.json()
-        .then(jsonr => document.getElementById('rumour').innerHTML = JSON.parse(jsonr.body).replace(/"/g, "'"))
+      document.getElementById('rumour').innerHTML = response.replace(/"/g, "'")
     })
     .catch(err => console.error(err))
   };
