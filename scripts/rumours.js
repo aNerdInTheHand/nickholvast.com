92088a9bb103de8ae0eaae976652c4ced0948db5
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2022-06-14 15:10:55 +0100

    do not replace

--- scripts/rumours.js
+++ scripts/rumours.js
@@ -2,7 +2,7 @@ function getRumour () {
   fetch('https://00xzeci8z8.execute-api.eu-west-1.amazonaws.com')
     .then(response => {
       console.log(response)
-      document.getElementById('rumour').innerHTML = response.replace(/"/g, "'")
+      document.getElementById('rumour').innerHTML = response
     })
     .catch(err => console.error(err))
   };
