07056d04e16f8239d993518c88e90c4dc07ea750
Nick Holvast <nick.holvast@multiverse.io>
2023-06-23 17:04:33 +0100

    update cv

--- components/header.js
+++ components/header.js
@@ -8,16 +8,10 @@ class Header extends HTMLElement {
       <header class="header">
         <a class="header-link" href="/">nickholvast.com</a>
         <a class="header-link-short" href="/">NH</a>
-        <a class="header-link" href="/cv.html">CV</a>
-        <a class="header-link-short" href="/cv.html">CV</a>
-        <a class="header-link" href="/music.html">Music</a>
-        <a class="header-link-short" href="/music.html">M</a>
-        <a class="header-link" href="/football.html">Football</a>
-        <a class="header-link-short" href="/football.html">F</a>
-        <a class="header-link" href="/coding.html">Coding</a>
-        <a class="header-link-short" href="/coding.html">C</a>
-        <a class="header-link" href="/sully.html">Sully</a>
-        <a class="header-link-short" href="/sully.html">S</a>
+        <a class="header-link" href="/rumours.html">Come And Get Me Plea</a>
+        <a class="header-link-short" href="/rumours.html">Rumours</a>
+        <a class="header-link" href="/about.html">About</a>
+        <a class="header-link-short" href="/about.html">Me</a>
       </header>
     `;
   }
